0ff94a726b00630acf43f550a13631b1ca647ed0
hassaammgl <h455a4mmehtab@gmail.com>
2024-10-29 16:53:40 +0500

    adding 3d model

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import "./style.css";
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 import MacContainer from "./components/MacContainer.jsx";
 
 const App = () => {
 	return (
-		<Canvas>
-			<OrbitControls />
+		<Canvas camera={{ fov: 20, position: [0, -10, 220] }}>
+			{/* <OrbitControls /> */}
+			<Environment
+				files={[
+					"https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/2k/studio_small_09_2k.hdr",
+				]}
+			/>
 			<MacContainer />
 		</Canvas>
 	);
--- src/components/MacContainer.jsx
+++ src/components/MacContainer.jsx
@@ -1,5 +1,23 @@
+/* eslint-disable react/no-unknown-property */
+import { useGLTF } from "@react-three/drei";
+import * as THREE from "three";
+
 const MacContainer = () => {
-	return <div>MacContainer</div>;
+	const model = useGLTF("./mac.glb");
+
+	let meshes = {};
+	model.scene.traverse((e) => {
+		meshes[e.name] = e;
+	});
+	console.log(meshes);
+
+	meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
+
+	return (
+		<group position={[0, -10, 20]}>
+			<primitive object={model.scene} />;
+		</group>
+	);
 };
 
 export default MacContainer;
