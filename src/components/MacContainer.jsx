ceeb87333685ab124d9b6b69b000c9e21793aba6
hassaammgl <h455a4mmehtab@gmail.com>
2024-10-29 17:06:48 +0500

    fixing textures

--- src/components/MacContainer.jsx
+++ src/components/MacContainer.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/no-unknown-property */
-import { useGLTF, useScroll } from "@react-three/drei";
+import { useGLTF, useScroll, useTexture } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 
 const MacContainer = () => {
 	const model = useGLTF("./mac.glb");
-
+	const tex = useTexture("./red.jpg");
 	let meshes = {};
 	model.scene.traverse((e) => {
 		meshes[e.name] = e;
@@ -14,10 +14,14 @@ const MacContainer = () => {
 
 	meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
 
+	meshes.matte.material.map = tex;
+	meshes.matte.material.emissiveIntensity = 0;
+	meshes.matte.material.metalness = 0;
+	meshes.matte.material.roughness = 1;
+
 	let data = useScroll();
 
 	useFrame((state, delta) => {
-		console.log(data.offset);
 		meshes.screen.rotation.x = THREE.MathUtils.degToRad(
 			180 - data.offset * 90
 		);
